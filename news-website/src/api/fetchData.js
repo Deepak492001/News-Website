48e7d122a2d60087efb2b37b169f4e08aa867f4f
Deepak Bisht <deepak.bisht20010904@gmail.com>
2024-05-02 11:17:48 +0530

    KEY REMOVE

--- news-website/src/api/fetchData.js
+++ news-website/src/api/fetchData.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const BASE_URL =
-  "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=4a4f2552ae8f47b8bc80cee68cb27e1d";
+
 export const getData = async () => {
   try {
     const response = await axios.get(`${BASE_URL}`);
